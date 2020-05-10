5167e08da7b5798b7c2e920f9e6cabeb9aa7e199
Michael Currin <michael.currin@gmail.com>
2020-05-10 12:31:37 +0200

    docs: Update init

--- wip/lib/__init__.py
+++ wip/lib/__init__.py
@@ -3,6 +3,8 @@ Library initialization module.
 
 # TODO: Redo with pathlib and see if there is a better pattern for handling YAML.
 # Create conf module here, or etc?
+
+# TODO read credentials from anywhere.
 """
 import os
 
