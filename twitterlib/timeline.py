b2826388d28dacbc0608fdf94486e57d0a59c340
Michael Currin <michael.currin@gmail.com>
2020-05-10 13:38:54 +0200

    chore: Cleanup comments

--- twitterlib/timeline.py
+++ twitterlib/timeline.py
@@ -5,10 +5,6 @@ import sys
 
 import tweepy
 
-# credentials = auth.get_credentials()
-# token = auth.app_access_token(**credentials)
-# api = auth.setup_api(token)
-
 from auth import TwitterConnection
 
 
