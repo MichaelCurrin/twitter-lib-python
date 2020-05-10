aa672c890da8db874d9792b946405379e7f122c1
Michael Currin <michael.currin@gmail.com>
2020-05-10 13:32:49 +0200

    feat: Update auth.py

--- twitterlib/auth.py
+++ twitterlib/auth.py
@@ -4,44 +4,77 @@ import tweepy
 import os
 
 
-def get_credentials():
+def get_credentials_from_env():
     consumer_key = os.environ.get("CONSUMER_KEY")
     consumer_secret = os.environ.get("CONSUMER_SECRET")
 
     access_key = os.environ.get("ACCESS_KEY")
     access_secret = os.environ.get("ACCESS_SECRET")
 
-    return dict(
-        consumer_key=consumer_key,
-        consumer_secret=consumer_secret,
-        access_key=access_key,
-        access_secret=access_secret,
-    )
+    return consumer_key, consumer_secret, access_key, access_secret
 
 
-# Or get these internally or add a wrapper. TBD on structure.
-def app_access_token(consumer_key, consumer_secret, access_key, access_secret):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+class TwitterConnection:
+    def __init__(self):
+        # Avoid moving setup steps here until I know the flows.
+        # Maybe there are methods here or functions outside for one line setup
+        # flows.
+        self.auth = None
 
-    auth.set_access_token(access_key, access_secret)
+        self.consumer_key = None
+        self.consumer_secret = None
+        self.access_key = None
+        self.access_secret = None
 
-    return auth
+        self.connection_options = dict(
+            wait_on_rate_limit=True,
+            wait_on_rate_limit_notify=True,
+            retry_count=3,
+            retry_delay=5,
+            retry_errors=[401, 404, 500, 503],
+        )
+
+        self.api = None
+
+    def set_credentials(self):
+        (
+            consumer_key,
+            consumer_secret,
+            access_key,
+            access_secret,
+        ) = get_credentials_from_env()
+
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.access_key = access_key
+        self.access_secret = access_secret
+
+    def app_access_token(self):
+        self.auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        self.auth.set_access_token(self.access_key, self.access_secret)
+
+        return self.auth
+
+    def app_only_token(self):
+        self.auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+
+        return self.auth
+
+    def setup_api(self):
+        self.api = tweepy.API(self.auth, **self.connection_options)
+
+        return self.api
 
 
-def app_only_token(consumer_key, consumer_secret):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+def test():
+    conn = TwitterConnection()
 
-    return auth
+    conn.set_credentials()
+    conn.app_access_token()
+    api = conn.setup_api()
+
+    print(api.verify_credentials())
 
 
-def setup_api(auth):
-    api = tweepy.API(
-        auth,
-        wait_on_rate_limit=True,
-        wait_on_rate_limit_notify=True,
-        retry_count=3,
-        retry_delay=5,
-        retry_errors=[401, 404, 500, 503],
-    )
-
-    return api
+if __name__ == "__main__":
+    test()
