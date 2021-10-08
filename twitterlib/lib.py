ad755897770b2b41f3fae22c6ec06d8cd85ada4d
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-10-08 11:35:01 +0200

    update lib.py

--- twitterlib/lib.py
+++ twitterlib/lib.py
@@ -2,11 +2,17 @@
 Library module.
 """
 import csv
+from pathlib import Path
+
+
+TWEETS_CSV_PATH = Path(__file__).parent / "var" / "tweets.csv"
 
 
 def get_message(tweet):
     """
-    Get the message on a Tweet object.
+    Get the message attribute on a Tweet object.
+
+    The attribute available depends on the parameters on the query to Twitter.
     """
     try:
         message = tweet.full_text
@@ -31,18 +37,21 @@ def write_csv(filepath, rows):
     """
     Note this will overwrite and not append.
     """
+    print(f"Writing to {filepath}")
+
     with open(filepath, "w", encoding="utf-8") as f_out:
         writer = csv.writer(f_out)
         writer.writerows(rows)
 
 
-def tweets_to_csv(tweets):
+def tweets_to_csv(tweets, out_path=None):
     """
     Write out tweets to a CSV file.
     """
-    out_file = "var/tweets.csv"
+    if not out_path:
+        out_path = TWEETS_CSV_PATH
 
     out_tweets = [
         (tweet.id, tweet.author.screen_name, get_message(tweet)) for tweet in tweets
     ]
-    write_csv(out_file, out_tweets)
+    write_csv(out_path, out_tweets)
