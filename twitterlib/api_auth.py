06e1c50639380ed91481526ee6777bdcc2516f0d
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-10-08 11:56:19 +0200

    feat: add App-Only Token flow

--- twitterlib/api_auth.py
+++ twitterlib/api_auth.py
@@ -40,10 +40,10 @@ def get_credentials(use_access_tokens: bool):
 
     assert consumer_key and consumer_secret, "Consumers tokens must always be set"
 
-    if use_access_tokens:
-        access_key = os.environ.get("ACCESS_KEY")
-        access_secret = os.environ.get("ACCESS_SECRET")
+    access_key = os.environ.get("ACCESS_KEY")
+    access_secret = os.environ.get("ACCESS_SECRET")
 
+    if use_access_tokens:
         assert access_key and access_secret, "Access tokens must be set for chosen flow"
 
     credentials: Credentials = {
@@ -81,8 +81,28 @@ def setup_api(auth: tweepy.OAuthHandler) -> tweepy.API:
 def app_access_token_api() -> tweepy.API:
     """
     Get API object which has App Access Token auth.
+
+    This authentication method uses consumer and access credentials. This can
+    be used to get details about the current user - such as the owner of the
+    Twitter app.
     """
     auth = setup_auth(use_access_tokens=True)
+
+    return setup_api(auth)
+
+
+def app_only_token_api() -> tweepy.API:
+    """
+    Get API object which has App-Only Token auth.
+
+    This authentication method uses consumer credentials only. This has no
+    concept of "current user" and also will not work with
+    `api.verify_credentials()`.
+
+    The advantage is that this method has different rate limits - it is better
+    in some cases, such as searching for tweets.
+    """
+    auth = setup_auth(use_access_tokens=False)
 
     return setup_api(auth)
 
@@ -91,10 +111,17 @@ def main():
     """
     Command-line entry point to test API access.
     """
+    print("App Access Token method")
     api = app_access_token_api()
     resp = api.verify_credentials()
 
     print(f"Authenticated with API successfully as: {resp.screen_name}")
+    print()
+
+    print("App-Only Token method")
+    api = app_only_token_api()
+
+    print("Authenticated with API successfully")
 
 
 if __name__ == "__main__":
